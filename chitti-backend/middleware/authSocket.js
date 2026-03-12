d111919c340ac5b11b08158419be9fe63db93cd8
balarama756 <balaramak86@gmail.com>
2026-03-12 15:59:37 +0530

    fix: load dotenv before requires, fix port for Render" git push

--- chitti-backend/middleware/authSocket.js
+++ chitti-backend/middleware/authSocket.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: '../config.env' });
+dotenv.config({ path: './config.env' });
 
 const config = process.env;
 
--- chitti-backend/server.js
+++ chitti-backend/server.js
@@ -1,26 +1,22 @@
-const app = require('./app')
 const dotenv = require('dotenv');
-
-const mongoose = require('mongoose');
-const socketServer = require('./socketServer');
-
 dotenv.config({ path: './config.env' });
 
-const PORT = process.env.PORT || process.env.API_PORT;
-
+const app = require('./app');
+const mongoose = require('mongoose');
+const socketServer = require('./socketServer');
 const http = require('http');
+
+const PORT = process.env.PORT || 8000;
+
 const server = http.createServer(app);
 socketServer.registerSocketServer(server);
 
-//console.log(process.env.MONGO_URI)
-
 mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log('MongoDB connection succesful!')
+    console.log('MongoDB connection successful!');
     server.listen(PORT, () => {
-        console.log(`server is listening on ${PORT}`)
-    })
+        console.log(`Server is listening on ${PORT}`);
+    });
 }).catch((err) => {
-    console.log('databse connection failed. Server not started ')
+    console.log('Database connection failed. Server not started.');
     console.log(err);
-})
-
+});
